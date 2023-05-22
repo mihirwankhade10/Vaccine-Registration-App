024a62b98de9cef6730a588aaf1f859905b84616
Mihir Wankhade <abhiwankhaded67@gmail.com>
2023-05-22 16:28:34 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,14 @@
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/vaccine-registration');
+// MongoDB Atlas connection URI
+mongoose.Promise = global.Promise;
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, "Error connecting to Mongodb"));
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
-});
-
+// aquire connection if it is succesful
+const db = 'mongodb+srv://wankhadeabhi3:mihir123@cluster0.duv7med.mongodb.net/?retryWrites=true&w=majority';
+// connect from mongodb
+// mongoose.connect('mongodb://127.0.0.1:27017/Vaccine-Registration-Database');
+   mongoose.connect(db).then(() => {
+    console.log(`Connection is succesfull`);
+   }).catch((err) => console.log(`no Connection`));
 module.exports = db;
